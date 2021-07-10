93805fbdd782513e2547e034c76516b202a7b900
Mal <34745578+malafk@users.noreply.github.com>
2021-07-10 01:13:17 +0100

    Testing

--- src/main/java/lol/maltest/arenasystem/ArenaSystem.java
+++ src/main/java/lol/maltest/arenasystem/ArenaSystem.java
@@ -7,7 +7,7 @@ public final class ArenaSystem extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("hi");
     }
 
     @Override
